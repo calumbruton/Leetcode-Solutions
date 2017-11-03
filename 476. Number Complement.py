ada8504a7a69feea37b9afa14f967d305668687d
Calum Bruton <calumbruton@gmail.com>
2017-11-02 21:04:20 -0400

    Bit Manipulation

---
+++ 476. Number Complement.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        binary = str(bin(num))
+        complement = []
+        for char in binary[2:]:
+            if char == "1":
+                complement.append("0")
+            else: 
+                complement.append("1")
+        return int("".join(complement), 2)
