69ae52ef608400ea06feb580906195ca377b2fb6
Calum Bruton <calumbruton@gmail.com>
2017-11-09 00:43:37 -0500

    Array

--- 506. Relative Ranks.py
+++ 506. Relative Ranks.py
@@ -1,6 +1,16 @@
 class Solution(object):
     def findRelativeRanks(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[str]
-        """
+        # rank max 3 numbers 
+        order = sorted(nums, reverse=True)
+        medals =["Gold Medal","Silver Medal","Bronze Medal"]
+        
+        for i,num in enumerate(nums):
+            loc = order.index(num)
+            print(loc)
+            if loc in [0,1,2]:
+                nums[i] = medals[loc]
+            else:
+                nums[i] = str(loc+1)
+                
+        return nums
+        
